f30d747c25d8e85b59d5912de30d5f48b9532de9
Panicy <63492790@qq.com>
2019-03-10 12:14:58 +0800

    Tabscroll

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
 import 'contants.dart' show Constants,AppColors;
+import './Pages/PageList.dart';
 
 /*定义底部导航*/
 class NavigationIconView{
-  String _title; 
-  IconData _icon;
-  IconData _activeIcon;
   BottomNavigationBarItem item;
-
   //构造器
   NavigationIconView({Key key,String title,IconData icon,IconData activeIcon}):
-    _title=title,
-    _icon=icon,
-    _activeIcon=activeIcon,
     item=new BottomNavigationBarItem(
-      icon:Icon(icon,color: Color(AppColors.TabIconNormal),),
-      activeIcon:Icon(activeIcon,color: Color(AppColors.TabIconActive),),
+      icon:Icon(icon,),
+      activeIcon:Icon(activeIcon,),
       title:Text(title,style: TextStyle(
         fontSize: 14.0,
-        color: Color(AppColors.TabIconNormal)
       ),),
       backgroundColor:Colors.white
     );
@@ -34,9 +27,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  PageController _pageController; //定义PageController 可滑动部件
+  int _currentIndex=0;
+  List<Widget> _pages; //页面
   List<NavigationIconView> _navigationIconViews;
   void initState(){
       super.initState();
+      _pageController=PageController(initialPage: _currentIndex);
+      _pages=[
+        Container(
+          child: Text('1'),
+        ),
+        Container(
+          child: Text('2'),
+        ),
+        Container(
+          child: Text('3'),
+        ),
+        Container(
+          child: Text('4'),
+        ),
+      ];
       _navigationIconViews=[
         NavigationIconView(
           title: '微信',
@@ -88,13 +100,17 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final BottomNavigationBar botNavBar=BottomNavigationBar(
+      fixedColor: Color(AppColors.TabIconActive),//底部导航点击时的图标文字颜色
       items: _navigationIconViews.map((NavigationIconView view){
         return view.item;
       }).toList(),
-      currentIndex:0,
+      currentIndex:_currentIndex,
       type: BottomNavigationBarType.fixed,
       onTap: (int index){
-        print('666');
+        setState((){
+          _currentIndex=index;//点击底部导航切换
+          _pageController.animateToPage(_currentIndex,duration:Duration(milliseconds: 200),curve:Curves.easeOut );//滑动Body切换底部导航
+        });
       },
     );
     return Scaffold(
@@ -107,20 +123,76 @@ class _HomeScreenState extends State<HomeScreen> {
         )),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.search),
+            icon: Icon(Icons.search,color: Color(AppColors.AppBarTextColors),),
             onPressed: (){},
           ),
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: (){
+          // IconButton(
+          //   icon: Icon(Icons.add,color: Color(AppColors.AppBarTextColors),),
+          //   onPressed: (){
+          //   },
+          // )
+          PopupMenuButton( //下拉选择框
+            itemBuilder: (BuildContext ctx){
+              return <PopupMenuItem<String>>[
+                PopupMenuItem(
+                  child: _buildPopupmenuIten(0xe60e,'发起群聊'),
+                  value:'fqql'
+                ),
+                PopupMenuItem(
+                  child: _buildPopupmenuIten(0xe60e,'添加朋友'),
+                  value:'添加朋友'
+                ),
+                PopupMenuItem(
+                  child: _buildPopupmenuIten(0xe60e,'扫一扫'),
+                  value:'扫一扫'
+                ),
+                PopupMenuItem(
+                  child: _buildPopupmenuIten(0xe60e,'收付款'),
+                  value:'收付款'
+                ),
+                PopupMenuItem(
+                  child: _buildPopupmenuIten(0xe60e,'帮助与反馈'),
+                  value:'帮助与反馈'
+                ),
+              ];
             },
-          )
+            icon: Icon(Icons.add,color: Color(AppColors.AppBarTextColors),),
+            onSelected: (String selected){
+              print(selected);
+            },
+          ),
+        Container(width: 16.0,),
+
         ],
       ),
-      body: Container(
-        color: Colors.red,
+      body: PageView.builder( //Pageview 可滑动
+        itemBuilder: (BuildContext context,int index){
+          return _pages[index]; //页面显示第几个
+        },
+        controller: _pageController,
+        itemCount: _pages.length, //页面个数
+        onPageChanged: (int index){//滑动时改变底部导航状态
+          setState(() {
+           _currentIndex=index; 
+          });
+        },
       ),
       bottomNavigationBar: botNavBar,
     );
   }
+}
+
+
+// 点击加号弹出框
+_buildPopupmenuIten(int icon,String title){
+  return Row(
+    children: <Widget>[
+      Icon(IconData(
+        icon,
+        fontFamily:Constants.IconFontFamily
+      )),
+      Container(width: 16.0,),
+      Text(title)
+    ],
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'contants.dart' show AppColors;
 void main()=>runApp(new MaterialApp(
   title: 'WeChat',
   theme: ThemeData.light().copyWith(
-    primaryColor: Color(AppColors.AppBarColor)
+    primaryColor: Color(AppColors.AppBarColor),
+    cardColor: Color(AppColors.AppBarColor)// 下拉菜单框背景颜色
   ),
   home: HomeScreen(),
 ));
